fbce0a0ebfff0143d26baf2008b6b9359b6367f3
nazarovmikhail <nazarovmiha@yandex.ru>
2015-03-03 23:06:14 +0300

    without Bigdecimal

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -8,30 +8,25 @@ public class Exercise1 {
 
     public static void main(String[] args){
 
-//        final double e=1e-6;
-//
-////        double[] x= new double [3];
-////        double[] s= new double [3];
-////        double[] a= new double [3];
-//
-        int h=11;
+
+        int h=50;
         double[] x= new double [h];
         double[] z= new double [h];
 
         double a1=0.4;
         double b1=4.0;
-        double h1=(b1-a1)/5;
+        double h1=(b1-a1)/(h-1);
 
-        x[0] = new BigDecimal(a1).setScale(4, RoundingMode.HALF_UP).doubleValue();
+        x[0] = a1;
         //x[0]=a1;
 
         for(int i=1; i<h; i++){
-            x[i] = new BigDecimal(x[i-1]+h1).setScale(4, RoundingMode.HALF_UP).doubleValue();
+            x[i] = x[i-1]+h1;
             //x[i]=x[i-1]+h1;
         }
         for(int i=0; i<h; i++){
-            z[i] = new BigDecimal(ci(x[i])).setScale(6, RoundingMode.HALF_UP).doubleValue();
-            System.out.println(x[i]+" \t"+z[i]);
+            z[i] = ci(x[i]);
+            System.out.printf("%.2f %.6f" + "\n", x[i], z[i]);
         }
 
 //        x[0]=0.8;
@@ -65,33 +60,48 @@ public class Exercise1 {
         double[] f1= new double [k];
         double[] f2= new double [k];
 
-        double[] x2= new double [k];
-
-        for(int i=0; i<h; i++) {
-            x2[i] = ((b1 - a1) / 2) * Math.cos((2 * i + 1) / (2 * h) * Math.PI) + (b1 + a1) / 2;
-        }
 
 
-        x1[0] =  new BigDecimal(a1).setScale(4, RoundingMode.HALF_UP).doubleValue();
-        double h2 = new BigDecimal((b1-a1)/(k-1)).setScale(4, RoundingMode.HALF_UP).doubleValue();
+
+        x1[0] =  a1;
+        double h2 = (b1-a1)/(k-1);
 
         for(int i=1; i<k; i++){
-            x1[i] = new BigDecimal(x1[i-1]+h2).setScale(4, RoundingMode.HALF_UP).doubleValue();
+            x1[i] = x1[i-1]+h2;
         }
         for(int i=0; i<k; i++){
-            f[i] = new BigDecimal(lagrange(x1[i],x,z)).setScale(6, RoundingMode.HALF_UP).doubleValue();
-            f1[i] = new BigDecimal(ci(x1[i])).setScale(6, RoundingMode.HALF_UP).doubleValue();
-            f2[i] = new BigDecimal(Math.abs(f[i]-f1[i])).setScale(6, RoundingMode.HALF_UP).doubleValue();
-            System.out.println(x1[i]+" \t"+f[i]+"\t"+f1[i]+"\t"+f2[i]);
+            f[i] = lagrange(x1[i],x,z);
+            f1[i] = ci(x1[i]);
+            f2[i] = Math.abs(f[i]-f1[i]);
+            System.out.printf("%.2f %.6f %.6f %.6f" + "\n", x1[i],f[i],f1[i],f2[i]);
                     if(Math.abs(f2[i]) >= e1){
                         e1=f2[i];
                     }
             }
-        System.out.println(h2);
-        System.out.println(Math.abs(e1));
-        for(int i=0; i<k; i++){
-            System.out.println(x2[i]);
+        System.out.println();
+        double e2=0;
+        double[] x2= new double [k];
+        double[] f3= new double [k];
+        double[] f4= new double [k];
+        double[] f5= new double [k];
+
+        for(int i=0; i<k; i++) {
+            x2[i] = ((b1 - a1) / 2.0) * Math.cos(((2 * i + 1) * Math.PI)/(2*k+1)) + (b1 + a1) / 2;
         }
+        for(int i=0; i<k; i++){
+            f3[i] = lagrange(x2[i],x,z);
+            f4[i] = ci(x2[i]);
+            f5[i] = Math.abs(f3[i]-f4[i]);
+            System.out.printf("%.2f %.6f %.6f %.6f" + "\n", x2[i],f3[i],f4[i],f5[i]);
+            if(Math.abs(f5[i]) >= e2){
+                e2=f2[i];
+            }
+        }
+        System.out.printf("%.6f" + "\n", Math.abs(e1));
+        System.out.printf("%.6f" + "\n", Math.abs(e2));
+//        for(int i=0; i<k; i++){
+//            System.out.printf("%.2f" + "\n", x2[i]);
+//        }
 
     }
 
@@ -101,10 +111,10 @@ public class Exercise1 {
             double k=1;
             for(int j=0; j<y.length; j++){
                 if(j!=i){
-                    k = new BigDecimal(k*((arg-x[j])/(x[i]-x[j]))).setScale(4, RoundingMode.HALF_UP).doubleValue();
+                    k = k*((arg-x[j])/(x[i]-x[j]));
                 }
             }
-            result = new BigDecimal(result+(k*y[i])).setScale(6, RoundingMode.HALF_UP).doubleValue();
+            result = result+(k*y[i]);
         }
         return result;
     }
@@ -113,10 +123,10 @@ public class Exercise1 {
         final double e=1e-6;
         double a,q,y=0;
         int n=1;
-        a = new BigDecimal(-Math.pow(x,2)/4).setScale(6, RoundingMode.HALF_UP).doubleValue();
+        a = -Math.pow(x,2)/4;
         while(Math.abs(a)>e){
-            y = new BigDecimal(y+a).setScale(6, RoundingMode.HALF_UP).doubleValue();
-            q = new BigDecimal(-(Math.pow(x,2)*2*n)/(Math.pow(2*n+2,2)*(2*n+1))).setScale(6, RoundingMode.HALF_UP).doubleValue();
+            y = y+a;
+            q = -(Math.pow(x,2)*2*n)/(Math.pow(2*n+2,2)*(2*n+1));
             n++;
             a=a*q;
         }
